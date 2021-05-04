071a7aeffaf48b7f89a6ac293b7c5dbde7fc72a8
Nils Martel <nilsmartel@yahoo.de>
2021-05-04 18:30:27 +0200

    Reimplement mappings of input to actions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod render;
 use render::render;
 
-use termion::raw::IntoRawMode;
-use termion::event::{Key, Event};
-use termion::input::TermRead;
 use std::io::{stdin, stdout};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 pub type SimpleString = Vec<char>;
 pub type Data = Vec<SimpleString>;
@@ -13,7 +13,7 @@ pub type Data = Vec<SimpleString>;
 pub struct State {
     pub cursor: (usize, usize),
     pub mode: Mode,
-    pub command_history: Vec<Key>
+    pub command_history: Vec<Key>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -35,9 +35,44 @@ fn main() {
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
     for c in stdin.events().filter_map(|c| c.ok()) {
-        let evt = c;
-
         // here acceptors and actions would need to be coded into
+        match c {
+            Event::Key(Key::Char(ch)) => match ch {
+                '\n' if state.mode == Mode::Insert => {
+                    data.insert(state.cursor.1, Vec::new());
+                    state.cursor.1 += 1;
+                }
+                n if state.mode == Mode::Insert => {
+                    data[state.cursor.1].insert(state.cursor.0, n);
+                    state.cursor.0 += 1;
+                }
+                '\t' if state.mode == Mode::Insert => {
+                    state.mode = Mode::Normal;
+                }
+                'h' if state.mode == Mode::Normal => {
+                    state.cursor.0 -= 1;
+                }
+                'l' if state.mode == Mode::Normal => {
+                    state.cursor.0 += 1;
+                }
+                'j' if state.mode == Mode::Normal => {
+                    state.cursor.1 -= 1;
+                }
+                'k' if state.mode == Mode::Normal => {
+                    state.cursor.1 += 1;
+                }
+                'i' if state.mode == Mode::Normal => {
+                    state.mode = Mode::Insert;
+                }
+                'a' if state.mode == Mode::Normal => {
+                    state.cursor.0 += 1;
+                    state.mode = Mode::Insert;
+                }
+                'q' if state.mode == Mode::Normal => break,
+                _ => {}
+            },
+            _ => {}
+        }
 
         render(&mut stdout, &data, &state);
     }
@@ -46,7 +81,7 @@ fn main() {
 type Mapping = (Acceptor, Action);
 
 /// A test wether a pattern of user inputs matches    
-/// todo: this may as well be a trait, for a regex might often match these patterns 
+/// todo: this may as well be a trait, for a regex might often match these patterns
 type Acceptor = Fn(Key, State) -> bool;
 
 /// An action to perform, yielding new state and input
